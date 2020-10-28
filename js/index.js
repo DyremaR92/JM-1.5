52d2bf427901d2dd7690bd354b93d5f486331ef4
Alexey <dyremar92@gmail.com>
2020-10-28 18:36:40 +0300

    fix swiper (finally)

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-let swiper = new Swiper ('.container', {
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    clickable: true,
-    // slidesPerView: 'auto',
-    // spaceBetween : 32,
-});
+if (window.innerWidth < 768) {
+    let swiper = new Swiper ('.container', {
+        pagination: {
+            el: '.swiper-pagination'
+        },
+        clickable: true,
+        slidesPerView: 'auto',
+        spaceBetween : 16,
+    });
+}
+
